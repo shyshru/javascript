4057097e5e03fb6eaa78eda37f3a31b8632df5c8
shyshru <142917648+shyshru@users.noreply.github.com>
2023-12-17 19:15:21 +0000

    conversion

---
+++ 01_basics/3_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+//if suppose we take "33abc" in score and convert it to Number it will convert it and its value will be Nan =>not a number
+//"33abc"=>Nan
+//if suppose we take "null" in score and convert it to Number it will convert it and its value will be 0
+//if suppose we take "undefined" in score and convert it to Number it will convert it and its value will be Nan =>not a number
+//if suppose we take "true/false" in score and convert it to Number it will convert it and its value will be 1/0
+//if suppose we take "abc" in score and convert it to Number it will convert it and its value will be Nan =>not a number
+
+//boolean conversion
+let isLoggedIn =1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+//empty string "" => false
+//"abc" => true
+
+//string conversion
+let num=3
+let stringNum = String(num)
+console.log(stringNum)
+console.log(typeof stringNum)
