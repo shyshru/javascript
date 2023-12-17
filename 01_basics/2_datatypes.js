6bac7f6b9819450c4341c53c7e6f82289a3679ac
shyshru <142917648+shyshru@users.noreply.github.com>
2023-12-17 18:54:51 +0000

    datatypes

---
+++ 01_basics/2_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"//treat all JS code as newer version
+//alert(3+3) //we are using node js not browser
+console.log(3+3)//code readability should be high
+
+console.log("hey universe")
+
+//ecmaScript tc39.es (standards of javascript)
+//mdn
+let name= "Ankita"
+let age= 18
+let isLoggedIn = false
+let state = null
+
+//number => 2 to the power of 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null => standalone value
+//undefined =>
+//symbol
+
+
+
+
+//object
+console.log(typeof"Ankita")
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
