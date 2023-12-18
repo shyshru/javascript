e26b55db5e7de74d9278b448fbe656b3b8e67a09
shyshru <142917648+shyshru@users.noreply.github.com>
2023-12-18 18:40:31 +0000

    date

---
+++ 01_basics/7_dateandtime.js
@@ -0,0 +1,12 @@
+//Dates
+
+
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+
+let myCreatedDate = new Date(2023,0,3)
+console.log(myCreatedDate.toDateString())
