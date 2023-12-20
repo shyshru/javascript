7a29cc2b67aa04991c8953a75b04f576361ccd9c
shyshru <142917648+shyshru@users.noreply.github.com>
2023-12-20 12:44:08 +0000

    func

--- 03_basics/1_function.js
+++ 03_basics/1_function.js
@@ -35,3 +35,41 @@ function loginUserMessage(username){
 }
 console.log(loginUserMessage("faf"))
 console.log(loginUserMessage())
+
+// function calculateCartPrice(num1){
+//     return num1
+
+// }
+// console.log(calculateCartPrice(200,400,500))
+
+function calculateCartPrice(...num1){
+    return num1
+
+}
+console.log(calculateCartPrice(200,400,500))
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+
+}
+console.log(calculateCartPrice(200,400,300,450,800,900,500))
+
+const user ={
+    username:"shri",
+    age:20
+}
+function handleObject (anyObject){
+    console.log(`Username is ${anyObject.username} and age is ${anyObject.age}`)
+}
+// handleObject(user)
+handleObject({
+    username:"shri",
+    age:20
+
+})
+
+const myNewArray = [200,300,4000,500,600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray))
