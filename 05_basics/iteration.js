f3bdcb37871514dc3aadb07c5fdbf43c96a9b6af
shyshru <142917648+shyshru@users.noreply.github.com>
2023-12-27 06:16:17 +0000

    iteration

---
+++ 05_basics/iteration.js
@@ -0,0 +1,14 @@
+//for
+// for(let index=0; index<10; index++){
+//     const element= index
+//     console.log(element)
+// }
+// for(let i=0; i<=10; i++){
+//     console.log(`outer loop value ${i}`)
+//     for(let j=0; j<=4; j++){
+//         console.log(`inner loop value ${j}`)
+//     }
+// }
+
+//keywords
+
